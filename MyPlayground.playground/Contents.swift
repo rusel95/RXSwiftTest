082676dd5c90d9e8983cd07efdba26e29b8c44af
Admin <ruslanpopesku95@gmail.com>
2017-04-10 20:13:48 +0300

    play with playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,74 @@
+//: Playground - noun: a place where people can play
+
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+import RxSwift
+
+//example("just") { 
+//    
+//    let observable = Observable.just("Hello, RxSwift")
+//    
+//    observable.subscribe({ (event) in
+//        print(event)
+//    })
+//}
+//
+//example("of") { 
+//    
+//    let observable = Observable.of(1,2,3,4,5,6)
+//    observable.subscribe({ (event) in
+//        print(event)
+//    })
+//    
+//}
+
+//example("create") { 
+//    let items = [1,2,3,4,5]
+//    Observable.from(items).subscribe(onNext: { (event) in
+//        print(event)
+//    }, onError: { (error) in
+//        print(error)
+//    }, onCompleted: { 
+//        print("OK!")
+//    }, onDisposed: { 
+//        print("dispoded")
+//    })
+//}
+
+//example("disposable") { 
+//    let seq = [1,2,3]
+//    Observable.from(seq).subscribe({ (event) in
+//        print(event)
+//    })
+//    Disposables.create()
+//}
+//
+//example("dispose") { 
+//    let seq = [1,2,3]
+//    let subscription = Observable.from(seq)
+//    subscription.subscribe({ (event) in
+//        print(event)
+//    }).dispose()
+//}
+
+example("disposeBag") {
+    let disposeBag = DisposeBag()
+    let seq = [1,2,3]
+    let subscription = Observable.from(seq)
+    subscription.subscribe({ (event) in
+        print(event)
+    }).addDisposableTo(disposeBag)
+}
+
+
+
+
+
+
+
+
+
+
+
