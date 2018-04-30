19bfa733090e78d03e6ff756647573597bb5ce33
Ruslan Popesku <ruslanpopesku95@gmail.com>
2018-04-30 18:13:05 +0300

    test Publish Subject

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -6,14 +6,15 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 
 import RxSwift
 
-//example("just") { 
-//    
-//    let observable = Observable.just("Hello, RxSwift")
-//    
-//    observable.subscribe({ (event) in
-//        print(event)
-//    })
-//}
+example("just") { 
+    
+    let observable = Observable.just("Hello, RxSwift")
+    
+    observable.subscribe({ (event) in
+        print(event)
+    })
+}
+
 //
 //example("of") { 
 //    
@@ -24,28 +25,28 @@ import RxSwift
 //    
 //}
 
-//example("create") { 
+//example("create") {
 //    let items = [1,2,3,4,5]
 //    Observable.from(items).subscribe(onNext: { (event) in
 //        print(event)
 //    }, onError: { (error) in
 //        print(error)
-//    }, onCompleted: { 
+//    }, onCompleted: {
 //        print("OK!")
-//    }, onDisposed: { 
+//    }, onDisposed: {
 //        print("dispoded")
 //    })
 //}
 
-//example("disposable") { 
+//example("disposable") {
 //    let seq = [1,2,3]
 //    Observable.from(seq).subscribe({ (event) in
 //        print(event)
 //    })
 //    Disposables.create()
 //}
-//
-//example("dispose") { 
+
+//example("dispose") {
 //    let seq = [1,2,3]
 //    let subscription = Observable.from(seq)
 //    subscription.subscribe({ (event) in
@@ -53,14 +54,33 @@ import RxSwift
 //    }).dispose()
 //}
 
-example("disposeBag") {
-    let disposeBag = DisposeBag()
-    let seq = [1,2,3]
-    let subscription = Observable.from(seq)
-    subscription.subscribe({ (event) in
-        print(event)
-    }).addDisposableTo(disposeBag)
-}
+//example("disposeBag") {
+//    let disposeBag = DisposeBag()
+//    let seq = [1,2,3]
+//    let subscription = Observable.from(seq)
+//    subscription.subscribe({ (event) in
+//        print(event)
+//    }).addDisposableTo(disposeBag)
+//}
+
+//example("PublishSubject") {
+//    let disposable = DisposeBag()
+//
+//    let subject = PublishSubject<String>()
+//
+//    subject.subscribe({ (event) in
+//        print("First: ", event)
+//    }).addDisposableTo(disposable)
+//
+//    subject.on(Event<String>.next("Hello"))
+//    subject.onNext("swift")
+//
+//    subject.subscribe(onNext: { (event) in
+//        print("Second: ", event)
+//    }).addDisposableTo(disposable)
+//
+//    subject.onNext("My name is Ruslan")
+//}
 
 
 
